54fcbc060ee651367d8d95db7418077cc991ba24
Nelson Carvalho <nelsonfilth@hotmail.com>
2025-04-12 12:36:18 -0300

    resolvendo confliots

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from pathlib import Path
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -12,9 +12,8 @@ engine = create_engine(DATABASE_URL)
 
 app = FastAPI()
 
-# Servir frontend (build)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# Caminho para a pasta build do frontend (gerado com Vite)
+frontend_path = Path(__file__).resolve().parent / "frontend" / "build"
 
-@app.get("/")
-def serve_frontend():
-    return FileResponse("static/index.html")
+# Montar arquivos estáticos
+app.mount("/", StaticFiles(directory=frontend_path, html=True), name="static")
