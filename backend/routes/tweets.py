d44dbe6028f6ab3bce9e70cc30a2d6f991d2a9e8
Nelson Carvalho <nelsonfilth@hotmail.com>
2025-04-18 18:14:40 -0300

    ajuste conflitantes 7

--- backend/routes/tweets.py
+++ backend/routes/tweets.py
@@ -3,9 +3,10 @@ from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from backend.models import Tweet, User
-from schemas import TweetCreate, TweetOut, TweetUpdate
-from database import get_db
-from auth import get_current_user  # <- Usando função padrão para validar token JWT
+from backend.schemas import TweetCreate, TweetOut, TweetUpdate
+from backend.database import get_db
+from backend.auth import get_current_user
+
 
 router = APIRouter(prefix="/tweets", tags=["Tweets"])
 
